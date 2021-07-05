4803ea2f3f4e976d01a36063d879506b5026708d
Raven <79323158+the-anonymous-raven@users.noreply.github.com>
2021-07-05 08:35:01 +0000

    Update polytheme.sh

--- polytheme.sh
+++ polytheme.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
+kill_nec(){
+pkill polybar
+pkill picom
+}
+
 if [[ "$@" == "" || "$@" == "-h" ]];
 then
   echo "Enter the right arguments :"
   printf  '%s\n%s\n%s\n%s' "1: material_theme" "2: nordish_mac" "3: pure_nordish_material" "4: Gloom-Oned-Theme"
 elif [[ "$@" == "-1" ]];
 then
+  kill_nec
   sh ~/.config/polybar/polybar-themes/material_theme/launch.sh
 elif [[ "$@" == "-2" ]];
 then
+  kill_nec
   sh ~/.config/polybar/polybar-themes/nordish_mac/launch.sh
 elif [[ "$@" == "-3" ]];
 then
+  kill_nec
   sh ~/.config/polybar/polybar-themes/pure_nordish_material/launch.sh
 elif [[ "$@" == "-4" ]];
 then
+  kill_nec
   sh ~/.config/polybar/polybar-themes/Gloom-Oned-Theme/launch.sh
 else
   echo "Wrong arguments passed"
