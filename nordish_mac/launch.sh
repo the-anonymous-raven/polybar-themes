9a5cecf009f0e7492e1ea3b717969cdc226c50dc
Raven <79323158+the-anonymous-raven@users.noreply.github.com>
2021-07-05 07:48:51 +0000

    Update launch.sh

--- nordish_mac/launch.sh
+++ nordish_mac/launch.sh
@@ -1,3 +1,3 @@
 feh --bg-fill ~/.config/polybar/polybar-themes/nordish_mac/wall/true_incognito.png
-picom --config ~/.config/polybar/polybar-themes/picom/config &
-polybar example -c ~/.config/polybar/polybar-themes/nordish_mac/nordish_mac.config & 
+picom --config ~/.config/polybar/polybar-themes/nordish_mac/picom/config &
+polybar example -c ~/.config/polybar/polybar-themes/nordish_mac/polybar/nordish_mac.config & 
