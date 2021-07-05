4ff352ade46306b86681f988e431865d67d79770
Raven <79323158+the-anonymous-raven@users.noreply.github.com>
2021-07-05 07:37:49 +0000

    Update launch.sh

--- material_theme/launch.sh
+++ material_theme/launch.sh
@@ -1,3 +1,3 @@
-feh --bg-fill ~/.config/polybar/polybar-themes/material_themes/wallpapers/wallpaper.png
-picom --config ~/.config/polybar/polybar-themes/picom/picom.conf
-sh ~/.config/polybar/polybar-themes/material_themes/polybar/launch.sh 
+feh --bg-fill ~/.config/polybar/polybar-themes/material_theme/wallpapers/wallpaper.png
+picom --config ~/.config/polybar/polybar-themes/material_theme/picom/picom.conf
+sh ~/.config/polybar/polybar-themes/material_theme/polybar/launch.sh 
