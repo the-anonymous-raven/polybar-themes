efe57db27a1b842cbdcde093a76bc5545684c44c
Raven <79323158+the-anonymous-raven@users.noreply.github.com>
2021-07-05 06:58:39 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ POLYBAR_FOLDER=$HOME/.config/polybar
 [[ -d $POLYBAR_FOLDER ]] && git -C $POLYBAR_FOLDER clone https://github.com/the-anonymous-raven/polybar-themes && git -c $POLYBAR_FOLDER clone https://github.com/jbirnick/polybar-timer && exit 0
 mkdir -p $POLYBAR_FOLDER &&  git -C $POLYBAR_FOLDER clone https://github.com/the-anonymous-raven/polybar-themes && git -c $POLYBAR_FOLDER clone https://github.com/jbirnick/polybar-timer && exit 0
 echo "Some error occured" && exit 1
+
+#installing polytheme.sh to /usr/bin
+sudo cp $HOME/.config/polybar/polybar-themes/polytheme.sh /usr/bin/
+echo "Successfully installed, run polytheme.sh to learn more" && exit 0
