2544115d6a56d644828f817206ba53da5afd29c8
Raven <79323158+the-anonymous-raven@users.noreply.github.com>
2021-07-05 07:39:00 +0000

    Update launch.sh

--- material_theme/polybar/launch.sh
+++ material_theme/polybar/launch.sh
@@ -1,3 +1,3 @@
-polybar left -c ~/.config/polybar/left_window &
-polybar right -c ~/.config/polybar/right_window &
-polybar center -c ~/.config/polybar/center_workspace &
+polybar left -c ~/.config/polybar/polybar-themes/material_theme/polybar/left_window &
+polybar right -c ~/.config/polybar/polybar-themes/material_theme/polybar/right_system &
+polybar center -c ~/.config/polybar/polybar-themes/material_theme/polybar/center_workspace &
