91a0870fcc3ab62ca69c726749a235f611d472a2
Raven <79323158+the-anonymous-raven@users.noreply.github.com>
2021-07-05 07:28:27 +0000

    Update polytheme.sh

--- polytheme.sh
+++ polytheme.sh
@@ -3,7 +3,7 @@
 if [[ "$@" == "" || "$@" == "-h" ]];
 then
   echo "Enter the right arguments :"
-  printf  '%s\n%s\n%s\n%s' "1: material_theme" "2: nordish_mac" "3: pure_nordish_material" "4: Gloom-oned-Polybar"
+  printf  '%s\n%s\n%s\n%s' "1: material_theme" "2: nordish_mac" "3: pure_nordish_material" "4: Gloom-Oned-Theme"
 elif [[ "$@" == "-1" ]];
 then
   sh ~/.config/polybar/polybar-themes/material_theme/launch.sh
@@ -15,7 +15,7 @@ then
   sh ~/.config/polybar/polybar-themes/pure_nordish_material/launch.sh
 elif [[ "$@" == "-4" ]];
 then
-  sh ~/.config/polybar/polybar-themes/Gloom-Oned-Polybar/launch.sh
+  sh ~/.config/polybar/polybar-themes/Gloom-Oned-Theme/launch.sh
 else
   echo "Wrong arguments passed"
   echo "Run polytheme.sh without arguments for help"
